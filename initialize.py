ea435f13e889784ac10cc7ea29a22d8720258a20
hashem78 <hashem.olayano@gmail.com>
2020-03-17 20:19:09 +0200

    Moar features

--- initialize.py
+++ initialize.py
@@ -1,17 +1,12 @@
 import time
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 
 import constants
 
-def execute():
-    # Set up selenium
-    driver = webdriver.Chrome(constants.DRIVER)
-
-
+def execute(driver):
+    
     # Navigate to website
     driver.get(constants.WEBSITE)
 
--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-import initialize
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
 
-initialize.execute()
+import time
+import initialize
+import constants
+
+# Set up selenium
+driver = webdriver.Chrome(constants.DRIVER)
+
+def navigate_to(path):
+    driver.get(path)
+    time.sleep(3)
+
+def add_post(course_tag,type="Notes",title = "Default title",content = "Place Holder"):
+    navigate_to("https://nerdat.net/course/"+course_tag)
+    write_post_button = driver.find_element_by_xpath("//*[@id='app']/div/main/div/div/div[1]/a[1]")
+    write_post_button.click()
+    time.sleep(3)
+    # Set note type
+    type_dropdown = driver.find_element_by_xpath("//*[@id='app']/div/main/div/form/div/article/div[1]/div[2]/div[1]/div/select")
+    Select(type_dropdown).select_by_visible_text(type)
+
+    # Fill title
+    course_title = driver.find_element_by_xpath("//*[@id='app']/div/main/div/form/div/article/div[1]/div[2]/div[2]/input")
+    course_title.send_keys(title)
+
+    # Fill content
+    course_content = driver.find_element_by_xpath("//*[@id='app']/div/main/div/form/div/article/div[1]/div[3]/div/div/p")
+    course_content.send_keys(content)
+
+    time.sleep(5)
+    # Publish content
+    course_publish = driver.find_element_by_xpath("//*[@id='app']/div/main/div/form/div/div/button[1]")
+    course_publish.click()
+
+def main():
+    initialize.execute(driver)
+    add_post("CPE200","Notes","This is a test title","This is test content")
+main()
